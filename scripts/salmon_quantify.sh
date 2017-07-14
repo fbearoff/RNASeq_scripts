7dc14a5bbba2372f7c02f69428c04cd45edcc2d6
Frank Bearoff <fbearoff@users.noreply.github.com>
2017-07-14 13:08:00 -0400

    add salmon script

---
+++ scripts/salmon_quantify.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+################################################################################
+#Created by Frank Bearoff fb99@drexel.edu                                      #
+#Drexel University College of Medicine Department of Microbiology & Immunology #
+################################################################################
+
+read_files="/home/frank/data/first_rnaseq/6LibbysRNAs"
+destination_directory="/home/frank/R_projects/rnaseq2"
+read_type="SR"
+transcriptome="/home/frank/GRCm38_construct/Mus_musculus.GRCm38.cdna.all.fa.gz"
+
+# Create index of transcriptome
+#k=31 is recommended value for reads of 75bp or longer
+salmon index -t "$transcriptome" -i "$destination_directory"/index --type quasi -k 31
+
+#quantify transcripts against index
+#This script assumes single-end reads with -r flag
+for sample in "$read_files"/*.gz;
+do
+echo "Processing sample ${sample#*/}"
+salmon quant -i "$destination_directory"/index/ -l "$read_type" -r "${sample}" -o "$destination_directory"/quants/"${sample#*/}"_quant
+done
