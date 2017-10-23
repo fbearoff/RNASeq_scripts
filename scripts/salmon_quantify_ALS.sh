ccdb134cf7e28d6718011bc553eb9ed0263a3286
Frank Bearoff <fbearoff@users.noreply.github.com>
2017-10-23 14:04:47 -0400

    added ALS salmon script

---
+++ scripts/salmon_quantify_ALS.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+################################################################################
+#Created by Frank Bearoff                                                      #
+#Drexel University College of Medicine Department of Microbiology & Immunology #
+################################################################################
+
+read_files="/home/frank/data/ALS_RNASeq/blankenhornRNAseq9samples"
+destination_directory="/home/frank/R_projects/ALS_RNASeq"
+read_type="ISR"
+transcriptome="/home/frank/GRCm38_construct/Mus_musculus.GRCm38.cdna.all.fa.gz"
+
+# Create index of transcriptome
+#k=31 is recommended value for reads of 75bp or longer
+salmon index -t "$transcriptome" -i "$destination_directory"/index --type quasi -k 31
+
+#quantify transcripts against index
+for sample in "$read_files"/*.R1.fq.gz;
+do
+base_name="${sample##*/}"
+echo "Processing sample ${base_name%%.R1*}"
+salmon quant -i "$destination_directory"/index/ -l $read_type -1"${sample}" -2 "${sample%%.R1*}.R2.fq.gz" -o "$destination_directory"/quants/"${base_name%%.R1*}"
+done
