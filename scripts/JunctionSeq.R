dac7dfe3064a57810b8eb091246ce9ae9547cf8d
Frank Bearoff <fbearoff@users.noreply.github.com>
2018-05-03 11:17:04 -0400

    add wiggle track creation

--- scripts/JunctionSeq.R
+++ scripts/JunctionSeq.R
@@ -1,14 +1,16 @@
 # Inputs ------------------------------------------------------------------
-QC_dir <- "/home/frank/analyzed/ALS_RNASeq2_call_variants/QC"
-WD <- "/scratch/frank/R_projects/ALS_RNASeq2"
-condition1 <- "jax_BB60"
-condition2 <- "SNO24"
-cores = 32
+QC_dir <- "/home/frank/analyzed/RNASeq1_call_variants/QC"
+WD <- "/home/frank/R_projects/RNASeq1"
+gene_names <- "/home/frank/GRCm38_construct/gene_names.csv"
+condition1 <- "B6"
+condition2 <- "103"
+cores = 24
 # Load Libraries ----------------------------------------------------------
 library(JunctionSeq)
 library(BiocParallel)
 # Load samples ------------------------------------------------------------
 setwd(WD)
+gene_names <- read.csv(gene_names, header = TRUE)
 comparison <- paste(condition1, "_vs_", condition2, sep = "")
 samples <- read.table(file.path(WD, "samples.txt"),
                       header = TRUE)
@@ -16,30 +18,34 @@ samples <-
   samples[which(samples$condition == condition1 |
                   samples$condition == condition2),]
 GFF <- file.path(QC_dir, "withNovel.forJunctionSeq.gff.gz")
-
 countFiles <-
   file.path(
     QC_dir,
     samples$sample.ID,
     "QC.spliceJunctionAndExonCounts.withNovel.forJunctionSeq.txt.gz"
   )
-
-
+# JunctionSeq -------------------------------------------------------------
 jscs <- runJunctionSeqAnalyses(
   sample.files = countFiles,
   sample.names = samples$sample.ID,
   condition = factor(samples$condition),
   flat.gff.file = GFF,
   nCores = cores,
-  analysis.type = "junctionsAndExons"
+  analysis.type = "junctionsAndExons",
+  gene.names = gene_names
 )
 
-dir.create(path =  paste0(WD, "/JunctionSeq/", comparison),
+dir.create(path =  paste0(WD, "/JunctionSeq/", comparison, "/results"),
            recursive = TRUE)
+
+writeCompleteResults(jscs,
+                     outfile.prefix = paste0(WD,"/JunctionSeq/", comparison, "/results/"),
+                     save.jscs = TRUE)
 
 buildAllPlots(
   jscs = jscs,
-  outfile.prefix = paste(WD,"/JunctionSeq/", comparison, "/plots/", sep = ""),
+  outfile.prefix = paste0(WD,"/JunctionSeq/", comparison, "/"),
   use.plotting.device = "png",
-  FDR.threshold = 0.01
+  FDR.threshold = 0.01,
+  colorRed.FDR.threshold = 0.05
 )
--- scripts/prep_JunctionSeq.sh
+++ scripts/prep_JunctionSeq.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-bam_folder="/scratch/frank/ALS_RNASeq2_call_variants/1-pass"
+bam_folder="/home/frank/analyzed/ALS_RNASeq2_call_variants/1-pass"
 fastq_folder="/home/frank/data/ALS_RNASeq2/20180302blankenhorn"
 samples_file="/home/frank/data/ALS_RNASeq2_samples.txt"
-output_folder="/home/frank/analyzed/ALS_RNASeq2_call_variants/QC"
+output_folder="/scratch/frank/analyzed/ALS_RNASeq2_call_variants/QC"
 decoder_file="/home/frank/R_projects/ALS_RNASeq2/samples.txt"
 fasta_ref="/home/frank/GRCm38_construct/Mus_musculus.GRCm38.dna.primary_assembly_space_stripped.fa"
+chrom_sizes="/home/frank/GRCm38_construct/Mus_musculus.GRCm38.dna.primary_assembly.fa.chrom_sizes"
 gtf_file="/home/frank/GRCm38_construct/Mus_musculus.GRCm38.88.gtf"
 qorts_location="/home/frank/bin/QoRTs.jar"
 
 #Read in samples
 declare -A aa_samples
-while IFS=: read -r key value; do
-    aa_samples[$key]+=$(echo "$value ")
+while IFS=: read -r genotype sample; do
+    aa_samples[$genotype]+=$(echo "$sample ")
 done < "$samples_file"
 
 echo "Samples are:"
-for key in "${!aa_samples[@]}"; do
-    echo "$key: ${aa_samples[$key]}"
+for genotype in "${!aa_samples[@]}"; do
+    echo "$genotype: ${aa_samples[$genotype]}"
 done
 
 read -p "Are these samples correct [Y/n]? " -n 1 -r
@@ -28,10 +29,13 @@ fi
 
 mkdir -p "$output_folder" || exit
 
+#Do QC on samples
 for sample in ${aa_samples[@]}; do
     echo "> Running QC on sample $sample"
     java -jar $qorts_location QC \
         --stranded \
+        --chromSizes "$chrom_sizes" \
+        --title "$sample" \
         --generatePlots \
         --genomeFA $fasta_ref \
         --rawfastq "$fastq_folder"/"$sample".R1.fq.gz,"$fastq_folder"/"$sample".R2.fq.gz \
@@ -39,6 +43,8 @@ for sample in ${aa_samples[@]}; do
         $gtf_file \
         "$output_folder"/"$sample"
 done
+
+#Generate novel junctions GFF for JunctionSeq
 java -jar $qorts_location \
         mergeNovelSplices \
         --minCount 6 \
@@ -47,3 +53,27 @@ java -jar $qorts_location \
         "$decoder_file" \
         "$gtf_file" \
         "$output_folder"
+
+#generate summary wiggle tracks
+mkdir -p "$output_folder"/mergedTracks
+for genotype in "${!aa_samples[@]}"; do
+    echo "Merging wiggle tracks for $genotype samples"
+    java -jar "$qorts_location" \
+        mergeWig \
+        --calcMean \
+        --infilePrefix "$output_folder"/ \
+        --infileSuffix /QC.wiggle.fwd.wig.gz \
+        --sampleList $(echo "${aa_samples[$genotype]}" |sed 's/\ /,/g' |sed 's/,*$//g') \
+        --sizeFactorFile "$output_folder"/JS.GEO.size.factors.txt \
+        --trackTitle "$genotype"_FWD \
+        "$output_folder"/mergedTracks/"$genotype".fwd.wig.gz
+    java -jar "$qorts_location" \
+        mergeWig \
+        --calcMean \
+        --infilePrefix "$output_folder"/ \
+        --infileSuffix /QC.wiggle.rev.wig.gz \
+        --sampleList $(echo "${aa_samples[$genotype]}" |sed 's/\ /,/g' |sed 's/,*$//g') \
+        --sizeFactorFile "$output_folder"/JS.GEO.size.factors.txt \
+        --trackTitle "$genotype"_REV \
+        "$output_folder"/mergedTracks/"$genotype".rev.wig.gz
+done
